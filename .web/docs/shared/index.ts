62452a5c2d511522ac08957d89439d5d3993d236
Robin <robin.braemer@web.de>
2022-11-28 12:06:36 +0100

    Update docs

--- .web/docs/shared/index.ts
+++ .web/docs/shared/index.ts
@@ -20,9 +20,9 @@ export const commitRef = process.env.CF_PAGES_COMMIT_SHA?.slice(0, 8) || 'dev'
 
 export const deployType = (() => {
     if (commitRef === '') {
-        return 'release'
+        return 'local'
     }
-    return 'local'
+    return 'release'
 })()
 
 export const additionalTitle = ((): string => {
