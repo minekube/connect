cb41487b26d6a876960590c0b5c23e6c54e02e9b
Robin Brämer <robin.braemer@web.de>
2024-01-23 16:17:32 +0100

    docs: hero logo motion (#40)

---
+++ .web/docs/.vitepress/theme/components/HeroAnimation.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="animation-container" class="relative h-128 w-128">
+    <div class="absolute left-1/2 animate-logo">
+      <img alt="Bottom Middle Node" src="/minekube-logo.png" width="128">
+    </div>
+    <div class="absolute left-1/2 top-64 animate-logo">
+      <img alt="Top Middle Node" src="/minekube-logo.png" width="128">
+    </div>
+    <div class="line animate-logo "></div>
+  </div>
+</template>
+
+<style scoped>
+@keyframes logo-animation {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(20px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.animate-logo {
+  animation: logo-animation 4s infinite ease-in-out;
+}
+
+@keyframes line-animation {
+  0% {
+    background-position: 0% 0%;
+  }
+  100% {
+    background-position: 100% 100%;
+  }
+}
+
+.line {
+  position: absolute;
+  top: 64px;
+  left: 50%;
+  width: 2px;
+  height: 64px;
+  background: linear-gradient(90deg, transparent, #d00808, transparent);
+  background-size: 200% 200%;
+  animation: line-animation 2s infinite;
+}
+</style>
+<script setup>
+</script>
---
+++ .web/docs/.vitepress/theme/components/HomeHeroImage.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Graph from "./Graph.vue";
+import VPImage from "./VPImage.vue";
+
+const velocity = 'https://avatars.githubusercontent.com/u/41710604?s=200&v=4'
+const paper = 'https://avatars.githubusercontent.com/u/7608950?s=200&v=4'
+const spigot = 'https://avatars.githubusercontent.com/u/4350249?s=48&v=4'
+
+const center = {x: 100, y: 0};
+const space = 100; // Define your space constant here
+
+const nodes = [
+  {x: center.x, y: center.y, link: '/guide/', image: '/minekube-logo.png', class: 'w-48'}, // Top center node
+  {x: center.x - space * 1.5, y: 200, link: '/guide/connectors/gate', image: '/gate.png', class: 'w-32'}, // First bottom node (Gate)
+  {x: center.x - space * 0.5, y: 250, link: '/guide/connectors/plugin', image: velocity}, // Second bottom node
+  {x: center.x + space * 0.5, y: 250, link: '/guide/connectors/plugin', image: paper, class: 'w-16'}, // Third bottom node
+  {x: center.x + space * 1.5, y: 200, link: '/guide/connectors/plugin', image: spigot}, // Fourth bottom node
+
+  // {x: center.x, y: 0, image: '/mascot.png', class: 'w-12'},
+
+];
+
+// Define connections between nodes by their index
+const connections = [
+  [0, 1],
+  [0, 2],
+  [0, 3],
+  [0, 4],
+];
+
+</script>
+
+<template>
+
+  <div class="hidden lg:block">
+    <Graph :connections="connections" :nodes="nodes"/>
+  </div>
+
+  <div class="block lg:hidden">
+    <VPImage class="image-src" image="/minekube-logo.png"/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
