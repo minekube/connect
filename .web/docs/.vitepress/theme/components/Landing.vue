992453e8a7f7b4c5362e6c8d0a45a74dbab241c2
robinbraemer <robin.braemer@web.de>
2024-10-21 09:41:37 +0200

    recover landing image

--- .web/docs/.vitepress/theme/components/Landing.vue
+++ .web/docs/.vitepress/theme/components/Landing.vue
@@ -171,7 +171,7 @@ import LogoCloud from "./LogoCloud.vue";
           <img alt="Product screenshot"
                class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
                height="1442"
-               src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" width="2432">
+               src="/images/dark-project-app-screenshot.png" width="2432">
         </div>
       </div>
     </div>
